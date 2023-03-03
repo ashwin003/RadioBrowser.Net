7b04e65424faac2ba0927831aa96360b1f7d65f2
Ashwin <12110372+ashwin003@users.noreply.github.com>
2023-03-03 11:03:10 +0530

    Adds a sample project

---
+++ example/RadioBrowserSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RadioBrowser.Net.Services;
+
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureServices(service => {
+    service.AddRadioBrowserServices("SampleApp/1.0.0");
+});
+var host = builder.Build();
+using var scope = host.Services.CreateScope();
+var serviceProvider = scope.ServiceProvider;
+var countryService = serviceProvider.GetRequiredService<ICountryService>();
+var countries = await countryService.FetchAsync();
+foreach (var country in countries) {
+    Console.WriteLine(country.Name);
+}
+Console.ReadKey();
